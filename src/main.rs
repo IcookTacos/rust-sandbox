f6a65422f176a11b7e69495d115cbe336cc6922f
IcookTacos <zeidlitzerik@gmail.com>
2023-10-20 14:20:39 +0200

    Chapter 2: Primitives

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod rustbyexample;
 use std::env;
 use std::process;
+use colored::Colorize;
 
 fn main(){
     let args: Vec<String> = env::args().collect();
@@ -15,7 +16,7 @@ fn main(){
 
 /*
  * It would be nice to make this dynamical, say I somehow keep track of what taks I've completed
- * and this help message adjustds the printout for what tasks are available
+ * and this help message adjusts the printout for what tasks are available
  */
 fn help_message(){
     println!("Chose what task you want to run (1-2) \nexample: cargo run --task1");
@@ -25,14 +26,28 @@ fn help_message(){
 fn run_task(task: String){
     println!("Running task");
     if task == "task1" { task1() }
-    println!("Unknown task");
-    process::exit(1);
+    if task == "task2" { task2() }
+    unknown_task();
 }
 
 fn task1(){
     rustbyexample::hello_world();
     rustbyexample::formatted_print();
     rustbyexample::debug_print();
-    println!("Task complete");
+    complete_task();
+}
+
+fn task2(){
+    rustbyexample::primitive_scalars();
+    complete_task();
+}
+
+fn complete_task(){
+    println!("{}","All tasks complete!".green());
+    process::exit(1);
+}
+
+fn unknown_task(){
+    println!("{} Unknown task","Error:".red());
     process::exit(1);
 }
--- src/rustbyexample.rs
+++ src/rustbyexample.rs
@@ -2,11 +2,14 @@ pub fn hello_world(){
     println!("Hello World");
 }
 
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
+
 pub fn formatted_print(){
     let x = 5;
     println!("x = {}", x);
     println!("This is arg0: {0} this is arg1: {1} here comes arg0 again: {0}", "a0", "a1");
-
     println!("Base 10               : {}",   69420); 
     println!("Base 2 (binary)       : {:b}", 69420); 
     println!("Base 8 (octal)        : {:o}", 69420); 
@@ -26,4 +29,25 @@ pub fn debug_print(){
     let age = 29;
     let peter = Person{name, age};
     println!("Using the debug to print person struct: \n{:#?}", peter);
+}
+
+pub fn primitive_scalars(){
+    let unsigned_integer: u8 = 20;
+    let signed_integer: i8 = -20;
+    let largest_unsinged_integer: u8 = 255;
+    println!("Unsigned 8 bit integer           : {}", unsigned_integer);
+    println!("Signed 8 bit integer             : {}", signed_integer);
+    println!("Largest unsinged 8 bit integer   : {}", largest_unsinged_integer);
+
+    let mut infered_type = 12;
+    print!("infered_type have type: ");
+    print_type_of(&infered_type);
+
+    infered_type = -5;
+    print!("infered_type have type: ");
+    print_type_of(&infered_type);
+}
+
+pub fn primitive_compounds(){
+    
 }
